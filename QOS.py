91406c5c06d0a49e21c018b365f5c9dd6cb27e11
S10275014 <S10275014@connect.np.edu.sg>
2026-08-05 11:42:57 +0800

    info update

--- QOS.py
+++ QOS.py
@@ -1,36 +1,52 @@
-print("=+=+=+=+=+=+=+= Welcome to Sea Bounty House =+=+=+=+=+=")
+import random
 
+print("=+=+=+=+=+=+=+= Welcome to Sea Bounty House =+=+=+=+=+=")
+option = -1
 NumberOfCust = 14
 
 waitingTime = 0
+while True:
+    print("There are about {} waiting in front of you".format(NumberOfCust))
+    print("Today's special: 95% of guests ordered the grilled chicken.")
+    print("There will be a discount of about 5% if you spend a minimum of $50")
+    option = int(input("Choose your option (0 for exit())"))
+    if option == 1:
+        if NumberOfCust <= 5:
+            waitingTime = 8
+            print("The waiting time is 8 minutes")
 
-if NumberOfCust <= 5:
-    waitingTime = 8
-    print("The waiting time is 8 minutes")
+        elif NumberOfCust <=10:
+            waitingTime = 12
+            print("The waiting time is 12 minutes")
 
-elif NumberOfCust <=10:
-    waitingTime = 12
-    print("The waiting time is 12 minutes")
+        elif NumberOfCust <=15:
+            waitingTime = 18
+            print("The waiting time is 18 minutes")
 
-elif NumberOfCust <=15:
-    waitingTime = 18
-    print("The waiting time is 18 minutes")
+        elif NumberOfCust <=20:
+            waitingTime = 24
+            print("The waiting time is 24 minutes")
 
-elif NumberOfCust <=20:
-    waitingTime = 24
-    print("The waiting time is 24 minutes")
+        elif NumberOfCust <=30:
+            waitingTime = 45
+            print("The waiting time is 45 minutes")
 
-elif NumberOfCust <=30:
-    waitingTime = 45
-    print("The waiting time is 45 minutes")
+        elif NumberOfCust <=40:
+            waitingTime = 50
+            print("The waiting time is about 50 minutes")
 
-elif NumberOfCust <=40:
-    waitingTime = 50
-    print("The waiting time is about 50 minutes")
+        else:
+            waitingTime = 60
+            print("The waiting time may be longer than an hour")
 
-else:
-    waitingTime = 60
-    print("The waiting time may be longer than an hour")
+        
+                
+
+
+
+
+
+
 
 
 
