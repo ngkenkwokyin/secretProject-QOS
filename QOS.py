e485377dc738b3d55ef1e0cc834e4d7196e48afd
S10275014 <S10275014@connect.np.edu.sg>
2026-08-05 17:48:10 +0800

    squash some bugs fixed minor errors for option 1

--- QOS.py
+++ QOS.py
@@ -1,73 +1,195 @@
 import random
-from datetime import datetime
+from datetime import datetime, timedelta # timedelta for time difference
+from time import sleep
 
-print("=+=+=+=+=+=+=+= Welcome to Sea Bounty House =+=+=+=+=+=")
+print("=+=+=+=+=+=+=+= Welcome to Sea Bounty House =+=+=+=+=+=+=+=")
 option = -1
-NumberOfCust = 14
+NumberOfCust = 14 #test data, can be ported from API
 
 waitingTime = 0
-while True:
-    print("There are about {} people queuing in front of you".format(NumberOfCust))
-    print()
-    print()
-    print("====== Discounts and Restaurant Insights =====")
-    print("Today's special: 95% of guests ordered the grilled squid.")
-    print("There will be a discount of about 5% if you spend a minimum of $50")
-    option = int(input("Choose your option (0 for exit())"))
-    if option == 0:
-        exit()
-    if option == 1:
-        if NumberOfCust <= 5:
-            waitingTime = 8
-            print("The waiting time is 8 minutes")
+table_status = "Not ready"
+ready_time = None
+extra_time = None
 
-        elif NumberOfCust <=10:
-            waitingTime = 12
-            print("The waiting time is 12 minutes")
+def waiting_status():
+    global waitingTime
+    global ready_time
+    global extra_time
+    global table_status
 
-        elif NumberOfCust <=15:
-            waitingTime = 18
-            print("The waiting time is 18 minutes")
+    choice = input("Do you want to wait for your table? (yes/no): ").lower()
 
-        elif NumberOfCust <=20:
-            waitingTime = 24
-            print("The waiting time is 24 minutes")
+    if choice == "yes":
+        if ready_time is None:
+            ready_time = datetime.now() + timedelta(minutes=waitingTime)
+            
 
-        elif NumberOfCust <=30:
-            waitingTime = 45
-            print("The waiting time is 45 minutes")
-
-        elif NumberOfCust <=40:
-            waitingTime = 50
-            print("The waiting time is about 50 minutes")
+        elif datetime.now() >= ready_time:
+            print("🟡 Preparing your table")
 
         else:
-            waitingTime = 60
-            print("The waiting time may be longer than an hour")
+            print("🔴 You are still in the queue")
 
-    if option == 2:
-            print("========THE SOUP OF THE DAY=======")
-            today = datetime.today().weekday()
-            if today % 7 == 0:
-                print("The soup of the day is tomato soup")
-            elif today == 1:
-                print("The soup of the day is calm soup")
-            elif today == 2:
-                print("The soup of the day is seafood soup")
-            elif today % 3 == 0:
-                print("The soup of the day is miso soup")
-            elif today == 4:
-                print("The soup of the day is vegetable soup")
-            elif today == 5:
-                print("The soup of the day is tofu soup")
-            else:
-                print("The soup of the day is beef stew")
-            print()
-            print()
-        
+        if table_status == "Preparing your table":
+                sleep(600) # 10 minustes
+                extra_time = timedelta(minutes=10)
+                ready_time += extra_time
+                table_status = "Your table is ready"
+        if table_status == "Your table is ready":
+                print("🟢 Your table is ready")
+
+    elif choice == "no":
+        print("Returning to main menu...")
+        return main_menu()
+    
+
+def main_menu():
+    while True:
+        print()
+        print()
+        print("==== WAITING INFORMATION ====")
+        print("There are about {} people queuing in front of you".format(NumberOfCust))
+        print()
+        print()
+        print("====== Discounts and Restaurant Insights =====")
+        print("Today's special: 95% of guests ordered the grilled squid.")
+        print("There will be a discount of about 5% if you spend a minimum of $50")
+        print("First 20 customers will have a free ice-cream!")
+        print()
+        print()
+        print("================ SELECT YOUR OPTION ================")
+        print("Type 0 for exit the QOS terminal system, Type 1 for waiting status, Type 2 for soup of the day")
+        option = int(input("Choose your option (0 for exit())"))
+        if option == 0:
+            #break
+            exit()
+        if option == 1:
+            waiting_status()
+            if NumberOfCust <= 5:
+                waitingTime = 8
+                print("The waiting time is 8 minutes")
+                print("🔴 You are still in the queue")
+                ready_time = datetime.now() + timedelta(minutes=8)
+                while datetime.now() < ready_time:
+                    sleep(1) #count per second
+                
+                table_status = "Preparing your table"
+                print("🟡 Preparing your table")
+                sleep(600) # 10 mins
+                print("🟢 Your table is ready")
+                table_status = "Your table is ready"
+
+
+            elif NumberOfCust <=10:
+                waitingTime = 12
+                print("The waiting time is 12 minutes")
+                print("🔴 You are still in the queue")
+                ready_time = datetime.now() + timedelta(minutes=12)
+                while datetime.now() < ready_time:
+                                sleep(1)
+                table_status = "Preparing your table"
+                print("🟡 Preparing your table")
+                sleep(600)
+                print("🟢 Your table is ready")
+                table_status = "Your table is ready"
+                
                 
 
+            elif NumberOfCust <=15:
+                waitingTime = 18
+                print("The waiting time is 18 minutes")
+                print("🔴 You are still in the queue")
+                ready_time = datetime.now() + timedelta(minutes=18)
+                while datetime.now() < ready_time:
+                                            sleep(1)
+                table_status = "Preparing your table"
+                print("🟡 Preparing your table")
+                sleep(600) 
+                print("🟢 Your table is ready")
+                table_status = "Your table is ready"
 
+            elif NumberOfCust <=20:
+                waitingTime = 24
+                print("The waiting time is 24 minutes")
+                print("🔴 You are still in the queue")
+                ready_time = datetime.now() + timedelta(minutes=18)
+                while datetime.now() < ready_time:
+                                            sleep(1)
+                print("🟡 Preparing your table")
+                sleep(600) 
+                print("🟢 Your table is ready")
+                table_status = "Your table is ready"
+
+            elif NumberOfCust <=30:
+                waitingTime = 45
+                print("The waiting time is 45 minutes")
+                print("🔴 You are still in the queue")
+                ready_time = datetime.now() + timedelta(minutes=45)
+                while datetime.now() < ready_time:
+                                            sleep(1)
+                print("🟡 Preparing your table")
+                sleep(600) 
+                print("🟢 Your table is ready")
+                table_status = "Your table is ready"
+
+            elif NumberOfCust <=40:
+                waitingTime = 50
+                print("The waiting time is about 50 minutes")
+                print("🔴 You are still in the queue")
+                ready_time = datetime.now() + timedelta(minutes=50)
+                while datetime.now() < ready_time:
+                                            sleep(1)
+                table_status = "Preparing your table"
+                print("🟡 Preparing your table")
+                sleep(600)
+                print("🟢 Your table is ready")
+                table_status = "Your table is ready"
+
+            else:
+                waitingTime = 60
+                print("The waiting time may be longer than an hour")
+                print("🔴 You are still in the queue")
+                ready_time = datetime.now() + timedelta(minutes=60)
+                while datetime.now() < ready_time:
+                                            sleep(1)
+                table_status = "Preparing your table"
+                print("🟡 Preparing your table")
+                sleep(600)
+                print("🟢 Your table is ready")
+                table_status = "Your table is ready"
+        
+
+        if option == 2:
+                print("========THE SOUP OF THE DAY=======")
+                today = datetime.today().weekday()
+                if today % 7 == 0:
+                    print("The soup of the day is tomato soup")
+                elif today == 1:
+                    print("The soup of the day is calm soup")
+                elif today == 2:
+                    print("The soup of the day is seafood soup")
+                elif today % 3 == 0:
+                    print("The soup of the day is miso soup")
+                elif today == 4:
+                    print("The soup of the day is vegetable soup")
+                elif today == 5:
+                    print("The soup of the day is tofu soup")
+                else:
+                    print("The soup of the day is beef stew")
+                print()
+                print()
+        else:
+                print()
+                print()
+                print("===== ERROR MESSAGE =====")
+                print("Invalid Option")
+                continue
+        
+
+#start the program
+main_menu()
+        
+                
 
 
 
