6b6decba37ff2670a2cdafbe19968824e1c37f0d
S10275014 <S10275014@connect.np.edu.sg>
2026-08-14 16:57:11 +0800

    v2.2 improve user experinece if user wants to wait for their table

--- QOS.py
+++ QOS.py
@@ -54,6 +54,95 @@ def generate_voucher():
     chars = string.ascii_uppercase + string.digits
     return ''.join(secrets.choice(chars) for _ in range(8))
 
+def new_game(): 
+        print("SELECT Y for yes \nSELECT N for no")
+        print("You may win points that can exchange for a free ice cream if you play this game while waiting")
+        continue_input = input("Do you want to play a game? (Y/N)").upper()
+
+
+        char_list = ["Member", "Imposter", "Sheriff", "Survivor"]
+
+        if continue_input == "Y":
+                print("\t\t\t ===== NEW GAME ===== \t\t\t")
+                print("\n Type the full name of the role: \n Memeber \n Imposter \n Sheriff \n Survivor")
+                user_input = input("What is the role you choose: ").capitalize()
+                if user_input not in char_list:
+                        print("\t\t\t ===== ERROR MESSAHE ===== \t\t\t")
+                        print("Invalid role.")
+                else:
+                        computer_pick = random.choice(char_list)
+                        print("The mysterious user joined the game.")
+                        print("The mysterious user has picked a role.")
+                        print("SELECT Y for yes \nSELECT N for no")
+                        if user_input != "Sheriff" and user_input != "Imposter":
+                                new_user_input = input("Do you think the mysterious user is an imposter ? (Y/N)").upper()
+                        if computer_pick != "Imposter" and new_user_input == "N":
+                                print("You won!")
+                                print("You won a free ice cream.")
+                                voucher = generate_voucher()
+                                print("Your voucher is", voucher)
+                        elif computer_pick == "Imposter" and new_user_input == "Y":
+                                print("You won!")
+                                print("You won a free ice cream.")
+                                voucher = generate_voucher()
+                                print("Your voucher is", voucher)
+                        else:
+                                print("You lost! 😭")
+
+                        print("Your role: ", user_input)
+                        print("mysterious user role: ", computer_pick)
+
+                        if user_input == "Imposter":
+                                print("Night falls....")
+                                new_user_input = input("Do you want to kill the mysterious user  ? (Y/N)").upper()
+                                if computer_pick != "Imposter" and new_user_input == "N":
+                                        print("The mysterious man lives...")
+                                        print("He have suspected you and you have lost... 😭")
+                                elif computer_pick == "Imposter" and new_user_input == "Y":
+                                        print("Oops.... you killed your friend... 😭")
+                                        print("You have officially lost")
+                                elif computer_pick != "Imposter" and new_user_input == "Y":
+                                        print("You have decided to kill the mysterious man....")
+                                        print("His blood is in your hands....")
+                                        print("BUT you have officially won!")
+                                        print("Slient win....")
+                                        voucher = generate_voucher()
+                                print("Your voucher is", voucher)
+                                print("Your role: ", user_input)
+                                print("mysterious user role: ", computer_pick)
+
+                                
+                                
+
+                        if user_input == "Sheriff":
+                                print("Night falls....")
+                                print("You have 3 bullets.")
+                                print("SELECT Y for yes \nSELECT N for no")
+                                new_user_input = input("Do you want to shoot the mysterious user  ? (Y/N)").upper()
+                                if computer_pick != "Imposter" and new_user_input == "N":
+                                        print("You won!")
+                                        print("You have saved an innocent man.")
+                                        print("You won a free ice cream.")
+                                        voucher = generate_voucher()
+                                        print("Your voucher is", voucher)
+                                elif computer_pick == "Imposter" and new_user_input == "Y":
+                                        print("You won!")
+                                        print("You have elimated the threat.")
+                                        print("You won a free ice cream.")
+                                        voucher = generate_voucher()
+                                        print("Your voucher is", voucher)
+                                elif computer_pick != "Imposter" and new_user_input == "Y":
+                                        print("You lost! 😭")
+                                        print("YOU HAVE KILLED AN INNOCENT MAN.")
+                                else:
+                                        print("You lost! 😭")
+                                        print("THE MYSTERIOUS USER HAS ESCAPED...")
+                                print("Your role: ", user_input)
+                                print("mysterious user role: ", computer_pick)
+        if continue_input == "N":
+                print("OK, you can relax or listen to some music.")
+
+
 def main_menu():
     while True:
         
@@ -82,6 +171,8 @@ def main_menu():
                 waitingTime = 8
                 print("The waiting time is 8 minutes")
                 print("🔴 You are still in the queue")
+                if datetime.now():
+                                new_game()
                 ready_time = datetime.now() + timedelta(minutes=8)
                 while datetime.now() < ready_time:
                     sleep(1) #count per second
@@ -96,6 +187,8 @@ def main_menu():
                 waitingTime = 12
                 print("The waiting time is 12 minutes")
                 print("🔴 You are still in the queue")
+                if datetime.now():
+                                new_game()
                 ready_time = datetime.now() + timedelta(minutes=12)
                 while datetime.now() < ready_time:
                                 sleep(1)
@@ -111,6 +204,8 @@ def main_menu():
                 waitingTime = 18
                 print("The waiting time is 18 minutes")
                 print("🔴 You are still in the queue")
+                if datetime.now():
+                                new_game()
                 ready_time = datetime.now() + timedelta(minutes=18)
                 while datetime.now() < ready_time:
                                             sleep(1)
@@ -124,6 +219,10 @@ def main_menu():
                 waitingTime = 24
                 print("The waiting time is 24 minutes")
                 print("🔴 You are still in the queue")
+                if datetime.now():
+                                new_game()
+                if datetime.now():
+                   new_game()
                 ready_time = datetime.now() + timedelta(minutes=18)
                 while datetime.now() < ready_time:
                                             sleep(1)
@@ -136,6 +235,8 @@ def main_menu():
                 waitingTime = 45
                 print("The waiting time is 45 minutes")
                 print("🔴 You are still in the queue")
+                if datetime.now():
+                                new_game()
                 ready_time = datetime.now() + timedelta(minutes=45)
                 while datetime.now() < ready_time:
                                             sleep(1)
@@ -148,6 +249,8 @@ def main_menu():
                 waitingTime = 50
                 print("The waiting time is about 50 minutes")
                 print("🔴 You are still in the queue")
+                if datetime.now():
+                                new_game()
                 ready_time = datetime.now() + timedelta(minutes=50)
                 while datetime.now() < ready_time:
                                             sleep(1)
@@ -161,6 +264,8 @@ def main_menu():
                 waitingTime = 60
                 print("The waiting time may be longer than an hour")
                 print("🔴 You are still in the queue")
+                if datetime.now():
+                                new_game()
                 ready_time = datetime.now() + timedelta(minutes=60)
                 while datetime.now() < ready_time:
                                             sleep(1)
@@ -169,6 +274,9 @@ def main_menu():
                 sleep(600)
                 print("🟢 Your table is ready")
                 table_status = "Your table is ready"
+
+        
+        
         
 
         elif option == 2:
